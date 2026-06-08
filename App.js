3858b1feb2933d6eb225c1781ff79513d5743319
Azka Auliarahma <azkaauliarahma08@gmail.com>
2026-06-08 20:29:08 +0700

    update port

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ import {
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
-const API_BASE_URL = "http://localhost:8081/api";
+const API_BASE_URL = "http://localhost:8080/api";
 
 const DEFAULT_FORM_DATA = {
   id: "",
